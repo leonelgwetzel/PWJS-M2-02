839d49558ecf351cf1959699c552039d6fd41b35
leonelgwetzel <wetzel.leonel.gabriel@gmail.com>
2019-10-22 20:01:15 -0300

    Vinculando archivo JSON

--- producto.class.js
+++ producto.class.js
@@ -1,10 +1,11 @@
 	class Producto {
 		//Constructor <- Vital: recibe los datos y fabrica el objeto. (Solo puede haber un solo constructor) ... Los parametros mas importantes van primero.
-		constructor(n, s, p, d = true){//<-- En el caso de que disponible sea "undefined x el usuario" lo lee como un TRUE a menos que se especifique lo contrario.
+		constructor(n, s, p, i, d = true){//<-- En el caso de que disponible sea "undefined x el usuario" lo lee como un TRUE a menos que se especifique lo contrario.
 		//Atributos
 		this.nombre = n
 		this.stock = s
 		this.precio = p
+		this.imagen = i
 		this.disponible = d
 
 	}
@@ -68,21 +69,17 @@
 			console.log(datos)
 
 			if(datos instanceof Array){
-				//1)Crear un Array nuevo para guardar los objetos Producto
-				let productos = new Array()
-
 				//2) Recorrer el Array de Object para instanciar los objetos Producto
-				datos.forEach( item => {
+				let productos =  datos.map( item => {
 					//3) Instanciar objetos Producto cn los datos de cada Object
-					let producto = new Producto(item.nombre, item.stock, item.precio, item.disponible)
-					//4)G Guardar el objeto Producto instanciado en el Array nuevo
-					productos.push(producto)
+					let producto = new Producto(item.Nombre, item.Stock, item.Precio, item.Imagen)
+					return producto
 				})
 				//5) Retornar el Array nuevo una vez que se hayan instanciado todos los objetos Producto
-				return(productos)
+				return productos
 
 			}else if(datos instanceof Object){
-				let producto = new Producto(datos.nombre, datos.stock, datos.precio, datos.disponible)
+				return new Producto(datos.Nombre, datos.Stock, datos.Precio, datos.Imagen)
 			}else {
 				console.error("Ya fue... no convierto nada en Producto")
 			}
