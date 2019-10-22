e6dc8c3c1cd52093970f8bd025c69f51397499d3
leonelgwetzel <wetzel.leonel.gabriel@gmail.com>
2019-10-22 19:21:05 -0300

    Iniciando proyecto MercadoTech

---
+++ producto.class.js
@@ -0,0 +1,90 @@
+	class Producto {
+		//Constructor <- Vital: recibe los datos y fabrica el objeto. (Solo puede haber un solo constructor) ... Los parametros mas importantes van primero.
+		constructor(n, s, p, d = true){//<-- En el caso de que disponible sea "undefined x el usuario" lo lee como un TRUE a menos que se especifique lo contrario.
+		//Atributos
+		this.nombre = n
+		this.stock = s
+		this.precio = p
+		this.disponible = d
+
+	}
+
+		//Propiedades L/E (Lectura y escritura o getters y setters)
+		get Precio(){ //Mantiene la variable original intacta
+			return "US$D"+(this.precio * 1.21).toFixed(2) //<- toFixed(x) cantidad de decimlaes a mostrar
+		}
+
+		set Precio(value){
+			if(isNaN(value) != true ){ //<- != no esta vacio, no es nulo o no esta definido
+				this.precio = value
+			}else {
+				console.error("El valor del precio no puede ser texto")
+			}
+		}
+
+		set Disponible(value){
+
+			let estado = value ? "habilitado" : " deshabilitado"
+
+			if( value == this.disponible) {
+				alert("El producto ya esta " + estado)
+			}
+
+			return
+			
+
+			if( confirm(`Desea ${estado} el producto "${this.nombre}"`) ){
+				this.disponible = value
+			}
+
+		}
+
+		//Metodos de Instancia -> Primero hay que instanciar un Producto (El .this en "producto.class.js indica que es el objeto que invoco el Mostrar()")
+		Mostrar(){
+			if(this.disponible ){ // o if(!this.disponible) <-- el !niega lo anterior.
+				let color = "green"
+			} else {
+				let color = "red"
+			}
+
+			//Operador ternario
+			let color = this.disponible ? "green" : "red"
+
+
+			document.write(`<p style="color:${color}">Hay <strong>${this.stock}</strong> unid. de ${this.nombre} que valen <em>ARG ${this.precio}</em> c/u<p>`)
+		}
+
+		aplicarDescuento(valor){
+			let importe = (this.precio * valor) / 100
+			this.precio = this.precio - importe
+			document.write(`OtroProducto con descuento: <strong><em>${this.precio}</em></strong>`)
+		}
+
+		//Metodos de Clase ó Metodos estatáticos.
+		static parse(json){ //
+			//Acá hay que hacer magia para que se conviertan en objetos 'Producto'
+			let datos = JSON.parse(json)//<- De JSON a Object
+			console.log("Estos son los datos:")
+			console.log(datos)
+
+			if(datos instanceof Array){
+				//1)Crear un Array nuevo para guardar los objetos Producto
+				let productos = new Array()
+
+				//2) Recorrer el Array de Object para instanciar los objetos Producto
+				datos.forEach( item => {
+					//3) Instanciar objetos Producto cn los datos de cada Object
+					let producto = new Producto(item.nombre, item.stock, item.precio, item.disponible)
+					//4)G Guardar el objeto Producto instanciado en el Array nuevo
+					productos.push(producto)
+				})
+				//5) Retornar el Array nuevo una vez que se hayan instanciado todos los objetos Producto
+				return(productos)
+
+			}else if(datos instanceof Object){
+				let producto = new Producto(datos.nombre, datos.stock, datos.precio, datos.disponible)
+			}else {
+				console.error("Ya fue... no convierto nada en Producto")
+			}
+		}
+	} 
